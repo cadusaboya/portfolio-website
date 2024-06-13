398e544fcfd8d4a7b7870eb469882211afb3dd6b
cadusaboya <carlosepsaboya@gmail.com>
2024-06-13 16:39:44 -0400

    Added support to github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    basePath: '/portfolio-website',
+    assetPrefix: '/portfolio-website/',
+    // Add other configurations here
+  };
+  
+  export default nextConfig;
