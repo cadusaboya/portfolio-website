26b140a5d2c23573294fa871fde9e023a729d97c
cadusaboya <carlosepsaboya@gmail.com>
2024-06-17 19:35:41 -0400

    Improve responsiveness on really small screens(iOS)

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,20 @@
-import Header from '../components/Header'
-import Card from '../components/Card'
-import AboutMe from '../components/AboutMe'
+import Header from '../components/Header';
+import Card from '../components/Card';
+import AboutMe from '../components/AboutMe';
 import NewsList from '../components/NewsList';
 import ContactForm from '@/components/ContactForm';
 
 const Home = () => {
   return (
-    <body>
-      <div className="flex justify-center">
-        <Header />
-      </div>
-          
-      <div id="about" className="flex-col justify-center pt-28">
+    <div className="min-h-screen flex flex-col items-center">
+      <Header />
+      
+      <div id="about" className="flex flex-col items-center pt-28 px-4 sm:px-0">
         <AboutMe />
       </div>
 
       <h2 id="projects" className="text-2xl font-bold mt-20 mx-auto flex justify-center">Projects</h2>
-      <div className="flex flex-1 mt-10 justify-center space-x-20">
+      <div className="flex flex-wrap justify-center mt-10 gap-10 px-4 sm:px-0">
         <Card
           title="My portfolio website"
           description="This is a website I'm proud to have built as my 3rd project. I used Next and Tailwind to quickly build a good looking UI, I consider this a big improvement over my first project, while spending less time on it. This is the place where I can showcase my projects and skills earnt."
@@ -28,9 +26,7 @@ const Home = () => {
 
         <Card
           title="Factor"
-          description="Mobile app that charges a small fee to antecipate the payments of hospitals to doctors. 
-                      The doctor is able to receive his payment imediately and the hospital would pay back to the app. 
-                      It was an idea I had and developed from scratch."
+          description="Mobile app that charges a small fee to antecipate the payments of hospitals to doctors. The doctor is able to receive his payment immediately and the hospital would pay back to the app. It was an idea I had and developed from scratch."
           imageUrl="/factor.png"
           skills={['Mobile Development', 'React Native', 'Python (Django)', 'PostgreSQL', 'Docker', 'Git', 'Unit Test', 'CI/CD Pipeline',  'AWS ECR', 'AWS ECS', 'AWS RDS', 'Route 53']}
           githubUrl='https://github.com/cadusaboya/factor'
@@ -38,11 +34,10 @@ const Home = () => {
         />
       </div>
 
-      <div className="flex flex-1 mt-20 justify-center space-x-20">
+      <div className="flex flex-wrap justify-center mt-20 gap-10 px-4 sm:px-0">
         <Card
           title="MyRoutine"
-          description=" This was my final project in CS50 and the very 1st software I made. It's a Web application that makes the user's routine a gamefied experience, 
-                        earning points when succesfully completing tasks and leaderboards to show rankings."
+          description="This was my final project in CS50 and the very 1st software I made. It's a Web application that makes the user's routine a gamefied experience, earning points when successfully completing tasks and leaderboards to show rankings."
           imageUrl="/myroutine.png"
           skills={['Python (Flask)', 'HTML', 'CSS', 'Javascript', 'SQLite']}
           githubUrl='https://github.com/cadusaboya/myroutine'
@@ -60,20 +55,20 @@ const Home = () => {
       </div>
 
       <h2 id="blog" className="text-2xl font-bold mt-20 mx-auto flex justify-center">Recent Posts</h2>
-      <div className="mt-10">
+      <div className="mt-10 px-4 sm:px-0">
         <NewsList />
       </div>
-      
-      
 
       <h2 id="contact" className="text-2xl font-bold mt-20 mx-auto flex justify-center">Contact</h2>
-      <ContactForm />
+      <div className="px-4 sm:px-0">
+        <ContactForm />
+      </div>
 
       <footer className="relative flex items-center justify-center w-full text-black text-center p-4 mt-20">
-     &copy; Carlos Saboya 2024 <a href="Resume.pdf" download className="absolute right-4">Download my Resume</a>
+        &copy; Carlos Saboya 2024 <a href="Resume.pdf" download className="absolute right-4">Download my Resume</a>
       </footer>
-    </body>
-  )
+    </div>
+  );
 }
 
-export default Home
+export default Home;
