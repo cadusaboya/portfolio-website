a43b59f3b2536ade5e9b1f0ee4a8fbf371837efa
cadusaboya <carlosepsaboya@gmail.com>
2024-06-17 19:36:25 -0400

    Centered text in regular screens, improve responsiveness on really small screens(iOS)

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -4,27 +4,35 @@ import Image from 'next/image';
 
 const AboutMe = () => {
   return (
-    <div className="flex">
-        <Image src="/my-img.jpeg" alt="My image" width={300} height={300} className="h-64 w-auto mx-auto rounded-full border-4 border-white" />
-        <main className="flex flex-col mx-auto w-72 p-4">
-            <h1 className="text-4xl font-bold">Carlos Saboya</h1>
-            <p className="mt-4">Welcome to my portfolio website. I&apos;m a software engineer and I love seeing my code go from an idea to a working product.</p>
-        </main>
-        <main className="flex flex-col mx-auto w-72 pt-4 pl-4">
-            <h1 className="text-4xl font-bold">About me</h1>
-            <p className="mt-4"><span className="font-bold">Age:</span> 27</p>
-            <p><span className="font-bold">Location:</span> Brazil 🇧🇷</p>
-            <p><span className="font-bold">Languages:</span> Portuguese and English</p>
-            <p><span className="font-bold">Likes:</span> Web3, Investing, E-sports, Games, Gym, Soccer, Travel</p>
+    <div className="flex flex-col sm:flex-row items-center sm:items-start">
+      <div className="md:ml-20 flex-shrink-0">
+        <Image 
+          src="/my-img.jpeg" 
+          alt="My image" 
+          width={300} 
+          height={300} 
+          className="h-64 w-64 sm:w-auto mx-auto rounded-full border-4 border-white" 
+        />
+      </div>
+      <main className="flex flex-col mx-auto sm:w-72 p-4 text-center sm:text-left">
+        <h1 className="text-4xl font-bold">Carlos Saboya</h1>
+        <p className="mt-4">Welcome to my portfolio website. I&apos;m a software engineer and I love seeing my code go from an idea to a working product.</p>
+      </main>
+      <main className="flex flex-col mx-auto sm:w-72 pt-4 pl-4 text-center sm:text-left">
+        <h1 className="text-4xl font-bold">About me</h1>
+        <p className="mt-4"><span className="font-bold">Age:</span> 27</p>
+        <p><span className="font-bold">Location:</span> Brazil 🇧🇷</p>
+        <p><span className="font-bold">Languages:</span> Portuguese and English</p>
+        <p><span className="font-bold">Likes:</span> Web3, Investing, E-sports, Games, Gym, Soccer, Travel</p>
 
-            <div className="flex mt-2">
-                <a href="https://www.linkedin.com/in/carlossaboya/" target="_blank" rel="noopener noreferrer" className="mr-1"><FaLinkedin className="w-6 h-6" /></a>
-                <a href="https://www.instagram.com/cadusaboya_" target="_blank" rel="noopener noreferrer" className="mr-1"><FaInstagram className="w-6 h-6" /></a>
-                <a href="https://github.com/cadusaboya" target="_blank" rel="noopener noreferrer" className="mr-1"><FaGithub className="w-6 h-6" /></a>
-                <a href="https://medium.com/@carlosepsaboya" target="_blank" rel="noopener noreferrer" className="mr-1"><FaMedium className="w-6 h-6" /></a>
-                <a href="https://web.whatsapp.com/send?phone=+5591984147769" target="_blank" rel="noopener noreferrer"><FaWhatsapp className="w-6 h-6" /></a>
-            </div> 
-        </main>
+        <div className="flex justify-center sm:justify-start mt-2">
+          <a href="https://www.linkedin.com/in/carlossaboya/" target="_blank" rel="noopener noreferrer" className="mr-1"><FaLinkedin className="w-6 h-6" /></a>
+          <a href="https://www.instagram.com/cadusaboya_" target="_blank" rel="noopener noreferrer" className="mr-1"><FaInstagram className="w-6 h-6" /></a>
+          <a href="https://github.com/cadusaboya" target="_blank" rel="noopener noreferrer" className="mr-1"><FaGithub className="w-6 h-6" /></a>
+          <a href="https://medium.com/@carlosepsaboya" target="_blank" rel="noopener noreferrer" className="mr-1"><FaMedium className="w-6 h-6" /></a>
+          <a href="https://web.whatsapp.com/send?phone=+5591984147769" target="_blank" rel="noopener noreferrer"><FaWhatsapp className="w-6 h-6" /></a>
+        </div> 
+      </main>
     </div>
   )
 }
